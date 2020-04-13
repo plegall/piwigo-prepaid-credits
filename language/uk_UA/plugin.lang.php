bd62a7c18ded4e9baeb56b1e6a8f30b193282eb0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 17:13:58 +0200

    Add uk_UA, thanks to tb0hdan

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = 'вказати фіксовану ціну';
+$lang['times the base cost per photo'] = 'множник базової ціни для фото';
+$lang['yes, buy'] = 'так, придбати';
+$lang['Pay on Paypal.com'] = 'Сплатити на Paypal.com';
+$lang['PayPal account'] = 'Обліковий запис PayPal';
+$lang['Price per credit'] = 'Ціна за кредит';
+$lang['Remove credits'] = 'Видалити кредити';
+$lang['Selling credits'] = 'Продаж кредитів';
+$lang['Set price'] = 'Встановити ціну';
+$lang['Show %s lines'] = 'Показувати %s рядків';
+$lang['Showing %s to %s of %s lines'] = 'Показано від %s до %s з %s рядків';
+$lang['Thank you!'] = 'Дякуємо!';
+$lang['Users can buy credits'] = 'Користувачі можуть придбати кредити';
+$lang['You have %d credits left'] = 'У Вас залишилось %d кредитів';
+$lang['credit(s) per photo'] = 'кредит(-ів) за фото';
+$lang['credits'] = 'кредити';
+$lang['day(s)'] = 'день(-ів)';
+$lang['error while buying photo'] = 'помилка придбання фото';
+$lang['follow default price (%s credits currently)'] = 'слідкуйте за ціною за замовчуванням (%s кредитів наразі)';
+$lang['hour(s)'] = 'година(-ин)';
+$lang['minute(s)'] = 'хвилина(-ин)';
+$lang['no, cancel'] = 'ні, скасувати';
+$lang['open it'] = 'відкрити';
+$lang['%d credits taken from your account.'] = '%d кредитів було знято з Вашого облікового запису.';
+$lang['%u credits bought on %s (%s %s)'] = '%u кредитів було придбано на %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u кредитів було витрачено на %s (розмір %s)';
+$lang['(filtered from %s total lines)'] = '(відфільтровано з %s всього рядків) ';
+$lang['Add credits'] = 'Додати кредити';
+$lang['Buy'] = 'Придбати';
+$lang['Buy more credits on your profile page.'] = 'Придбати додатково кредитів на сторінці Вашого профілю.';
+$lang['Buy this photo'] = 'Придбати це фото';
+$lang['Credits history'] = 'Історія кредитів';
+$lang['Credits paid'] = 'Сплачені кредити';
+$lang['Credits spent'] = 'Витрачені кредити';
+$lang['Default cost'] = 'Вартість за замовчуванням';
+$lang['Details'] = 'Подробиці';
+$lang['Do you want to use %d credits to download this photo?'] = 'Чи хочете використати %d кредитів на завантаження цього фото?';
+$lang['Download available for'] = 'Завантаження доступне протягом';
+$lang['Download now'] = 'Завантажити зараз';
+$lang['Need more? Contact us to get more credits.'] = 'Потребуєте більше кредитів? Зверніться до нас.';
+$lang['No matching line found'] = 'Рядок зі співпадінням не знайдено';
+$lang['No worry!'] = 'Не хвилюйтесь!';
+$lang['Not enough credits!'] = 'Недостатньо кредитів!';
