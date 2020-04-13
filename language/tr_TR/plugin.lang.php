4b7f4394f3f622dd40319878fd9c2861c99db15c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 21:23:30 +0200

    Add tr_TR, thanks to Nakre

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['(filtered from %s total lines)'] = '(filtrelendi %s toplam satırdan)';
+$lang['follow default price (%s credits currently)'] = 'varsayılan fiyatı takip et (şu anda %s kredi)';
+$lang['%u credits bought on %s (%s %s)'] = '%u kredi satın alındı %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u kredi harcandı%s (size %s)';
+$lang['%d credits taken from your account.'] = 'Hesabınızdan %d kredi alındı.';
+$lang['Do you want to use %d credits to download this photo?'] = 'Bu resmi indirmek için %d kredi kullanmak istiyor musunuz?';
+$lang['Users can buy credits'] = 'Kullanıcılar kredi satın alabilir';
+$lang['You have %d credits left'] = 'Sadece %d krediniz kaldı';
+$lang['credit(s) per photo'] = 'her resmin kredisi';
+$lang['credits'] = 'krediler';
+$lang['day(s)'] = 'gün';
+$lang['error while buying photo'] = 'resim alırken hata oluştu';
+$lang['hour(s)'] = 'saat';
+$lang['minute(s)'] = 'dakika';
+$lang['no, cancel'] = 'hayır, iptal et';
+$lang['open it'] = 'aç';
+$lang['specific fixed price'] = 'belirli sabit fiyat';
+$lang['times the base cost per photo'] = 'resim başına temel maliyetin katları';
+$lang['yes, buy'] = 'evet, satın al';
+$lang['Show %s lines'] = '%s satırları göster';
+$lang['Showing %s to %s of %s lines'] = '%s satırdan %s den %s satıra kadar göster';
+$lang['Selling credits'] = 'Kredi satışı';
+$lang['Set price'] = 'Fiyat belirle';
+$lang['Thank you!'] = 'Teşekkürler';
+$lang['Not enough credits!'] = 'Yeterli kredi yok!';
+$lang['Pay on Paypal.com'] = 'Paypal.com\'da Öde';
+$lang['PayPal account'] = 'Paypal hesabı';
+$lang['Price per credit'] = 'Kredi başına fiyat';
+$lang['Remove credits'] = 'Kredileri kaldır';
+$lang['Buy more credits on your profile page.'] = 'Profil sayfanızdan daha fazla kredi satın alın.';
+$lang['Download available for'] = 'İndirilebilecek';
+$lang['Download now'] = 'Şimdi indir';
+$lang['Need more? Contact us to get more credits.'] = 'Daha fazlasına mı ihtiyaç var? Daha fazla kredi almak için bizimle iletişime geçin.';
+$lang['No matching line found'] = 'Eşleşen satır bulunamadı';
+$lang['No worry!'] = 'Endişelenme!';
+$lang['Credits paid'] = 'Ödenen krediler';
+$lang['Credits spent'] = 'Harcanan krediler';
+$lang['Default cost'] = 'Varsayılan maliyet';
+$lang['Details'] = 'Detaylar';
+$lang['Add credits'] = 'Kredi ekle';
+$lang['Buy'] = 'Satın al';
+$lang['Buy this photo'] = 'Bu resmi satın al';
+$lang['Credits history'] = 'Kredi geçmişi';
