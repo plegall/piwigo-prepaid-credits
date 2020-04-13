277fa59d0efe2ca4fe1e8e462d9f91791d101473
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 21:24:11 +0200

    Add sv_SE, thanks to adrian_vg

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Set price'] = 'Sätt pris';
+$lang['error while buying photo'] = 'fel vid köp av foto';
+$lang['Pay on Paypal.com'] = 'Betala på Paypal.com';
+$lang['Not enough credits!'] = 'Inte tillgängligt med krediter!';
+$lang['Need more? Contact us to get more credits.'] = 'Behövs mer? Kontakta oss för att erhålla fler krediter.';
+$lang['Do you want to use %d credits to download this photo?'] = 'Vill du använda %d krediter för att ladda ner detta foto?';
+$lang['Credits spent'] = 'Använda krediter';
+$lang['Credits paid'] = 'Krediter betalda';
+$lang['Credits history'] = 'Kredithistorik';
+$lang['%u credits bought on %s (%s %s)'] = '%u krediter köpta %s (%s %s)';
+$lang['%d credits taken from your account.'] = '%d krediter tagna från kontot.';
+$lang['No worry!'] = 'Oroa dig inte!';
+$lang['Download now'] = 'Ladda ner nu';
+$lang['Download available for'] = 'Nerladdning tillgänglig i';
+$lang['Details'] = 'Detaljer';
+$lang['Default cost'] = 'Standardkostnad';
+$lang['Buy this photo'] = 'Köp detta foto';
+$lang['Buy'] = 'Köp';
