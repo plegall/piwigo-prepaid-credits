4f81283499fdd1ae46f00ccacf04f33c38def47a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 21:23:58 +0200

    Add nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['hour(s)'] = 'u(ur)ren';
+$lang['minute(s)'] = 'minu(u)t(en)';
+$lang['no, cancel'] = 'nee, annuleer';
+$lang['open it'] = 'open het';
+$lang['specific fixed price'] = 'specifieke vaste prijs';
+$lang['times the base cost per photo'] = 'maal de basiskosten per foto';
+$lang['yes, buy'] = 'ja, koop';
+$lang['%d credits taken from your account.'] = '%d tegoeden genomen van jouw account.';
+$lang['Pay on Paypal.com'] = 'Betaal via Paypal.com';
+$lang['PayPal account'] = 'Paypal account';
+$lang['Price per credit'] = 'Prijs per tegoed';
+$lang['Remove credits'] = 'Verwijder tegoeden';
+$lang['Selling credits'] = 'Verkoop tegoeden';
+$lang['Set price'] = 'Stel de prijs';
+$lang['Thank you!'] = 'Dank je!';
+$lang['Users can buy credits'] = 'Gebruiker kan tegoeden kopen';
+$lang['You have %d credits left'] = 'Je hebt %d tegoeden over';
+$lang['credit(s) per photo'] = 'Tegoed(en) per foto';
+$lang['credits'] = 'tegoeden';
+$lang['day(s)'] = 'dag(en)';
+$lang['error while buying photo'] = 'fout tijdens koop foto';
+$lang['Add credits'] = 'Voeg tegoeden toe';
+$lang['Buy'] = 'Koop';
+$lang['Buy more credits on your profile page.'] = 'Koop meer tegoeden op je profielpagina.';
+$lang['Buy this photo'] = 'Koop deze foto';
+$lang['Credits history'] = 'Tegoeden geschiedenis';
+$lang['Credits paid'] = 'Tegoeden betaald';
+$lang['Credits spent'] = 'Tegoeden besteed';
+$lang['Default cost'] = 'Standaardkosten';
+$lang['Details'] = 'Details';
+$lang['Do you want to use %d credits to download this photo?'] = 'Wil je %d tegoeden om deze foto te downloaden?';
+$lang['Download available for'] = 'Download beschikbaar voor';
+$lang['Download now'] = 'Download nu';
+$lang['Need more? Contact us to get more credits.'] = 'Meer nodig? Neem contact met ons op voor meer tegoeden.';
+$lang['No matching line found'] = 'Geen passende regel gevonden';
+$lang['No worry!'] = 'Geen zorg!';
+$lang['Not enough credits!'] = 'Niet genoeg tegoeden!';
