567c093b04330652f4fc9b7478cc03bb9743e8f3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 21:23:44 +0200

    Add da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You have %d credits left'] = 'Du har %d kreditter tilbage';
+$lang['specific fixed price'] = 'specifik fast pris';
+$lang['times the base cost per photo'] = 'gange grundbeløb pr. fotografi';
+$lang['yes, buy'] = 'ja, køb';
+$lang['Pay on Paypal.com'] = 'Betal på Paypal.com';
+$lang['PayPal account'] = 'PayPal-konto';
+$lang['Price per credit'] = 'Pris pr. kredit';
+$lang['Remove credits'] = 'Fjern kreditter';
+$lang['Selling credits'] = 'Sælg kreditter';
+$lang['Set price'] = 'Fastsæt pris';
+$lang['Show %s lines'] = 'Vis %s linjer';
+$lang['Showing %s to %s of %s lines'] = 'Viser %s til %s af %s linjer';
+$lang['Thank you!'] = 'Tak!';
+$lang['Users can buy credits'] = 'Brugere kan købe kreditter';
+$lang['credit(s) per photo'] = 'kredit(ter) pr. fotografi';
+$lang['credits'] = 'kreditter';
+$lang['day(s)'] = 'dag(e)';
+$lang['error while buying photo'] = 'fejl ved køb af fotografi';
+$lang['follow default price (%s credits currently)'] = 'følg standardpris (aktuelt %s kreditter)';
+$lang['hour(s)'] = 'time(r)';
+$lang['minute(s)'] = 'minut(ter)';
+$lang['no, cancel'] = 'nej, afbryd';
+$lang['open it'] = 'åbn det';
+$lang['%d credits taken from your account.'] = '%d kreditter trukket fra din konto.';
+$lang['%u credits bought on %s (%s %s)'] = '%u kreditter købt den %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u kreditter brugt på %s (størrelse %s)';
+$lang['(filtered from %s total lines)'] = '(filteret fra %s totale linjer)';
+$lang['Add credits'] = 'Tilføj kreditter';
+$lang['Buy'] = 'Køb';
+$lang['Buy more credits on your profile page.'] = 'Køb flere kreditter på din profilside';
+$lang['Buy this photo'] = 'Køb dette fotografi';
+$lang['Credits history'] = 'Forbrug af kreditter';
+$lang['Credits paid'] = 'Betalte kreditter';
+$lang['Credits spent'] = 'Brugte kreditter';
+$lang['Default cost'] = 'Standardomkostning';
+$lang['Details'] = 'Detaljer';
+$lang['Do you want to use %d credits to download this photo?'] = 'Ønsker du at bruge %d kreditter på at downloade dette fotografi?';
+$lang['Download available for'] = 'Download tilgængelig i';
+$lang['Download now'] = 'Download nu';
+$lang['Need more? Contact us to get more credits.'] = 'Har du ikke nok? Kontakt os for at få fat i flere kreditter.';
+$lang['No matching line found'] = 'Ingen tilsvarende linje fundet';
+$lang['No worry!'] = 'Bare rolig!';
+$lang['Not enough credits!'] = 'Utilstrækkelige kreditter!';
