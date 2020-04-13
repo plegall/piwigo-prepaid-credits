4ce9470a95098e7a7df464c5c4cbc7446aa32c0e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 17:14:13 +0200

    Add pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = 'preço fixo especifico ';
+$lang['times the base cost per photo'] = 'vezes o custo base por foto';
+$lang['yes, buy'] = 'sim, comprar';
+$lang['Pay on Paypal.com'] = 'Pague com Paypal.com';
+$lang['PayPal account'] = 'Conta Paypal';
+$lang['Price per credit'] = 'Preço por crédito';
+$lang['Remove credits'] = 'Remover créditos';
+$lang['Selling credits'] = 'Vendendo créditos';
+$lang['Set price'] = 'Configurar preço';
+$lang['Show %s lines'] = 'Mostrar %s linhas';
+$lang['Showing %s to %s of %s lines'] = 'Mostrando %s para %s de %s linhas';
+$lang['Thank you!'] = 'Obrigado!';
+$lang['Users can buy credits'] = 'Usuários podem comprar créditos';
+$lang['You have %d credits left'] = 'Você tem %d créditos de saldo';
+$lang['credit(s) per photo'] = 'Credito(s) por foto';
+$lang['credits'] = 'créditos';
+$lang['day(s)'] = 'dia(s)';
+$lang['error while buying photo'] = 'erro enquanto comprava foto';
+$lang['follow default price (%s credits currently)'] = 'siga o preço padrão (%s créditos atualmente)';
+$lang['hour(s)'] = 'hora(s)';
+$lang['minute(s)'] = 'minuto(s)';
+$lang['no, cancel'] = 'não, cancelar';
+$lang['open it'] = 'abrir';
+$lang['%d credits taken from your account.'] = '%d créditos retirados da sua conta.';
+$lang['%u credits bought on %s (%s %s)'] = '%u créditos comprados em %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u créditos gastos para %s (tamanho %s)';
+$lang['(filtered from %s total lines)'] = '(filtrado de %s linhas totais)';
+$lang['Add credits'] = 'Adicionar crédito';
+$lang['Buy'] = 'Comprar';
+$lang['Buy more credits on your profile page.'] = 'Compre mais créditos na sua página de perfil.';
+$lang['Buy this photo'] = 'Comprar esta foto';
+$lang['Credits history'] = 'Histórico de crédito';
+$lang['Credits paid'] = 'Créditos pagos';
+$lang['Credits spent'] = 'Créditos gastos';
+$lang['Default cost'] = 'Custo padrão';
+$lang['Details'] = 'Detalhes';
+$lang['Do you want to use %d credits to download this photo?'] = 'Você deseja usar %d créditos para baixar esta foto?';
+$lang['Download available for'] = 'Baixar disponível para';
+$lang['Download now'] = 'Baixar agora';
+$lang['Need more? Contact us to get more credits.'] = 'Preciso de mais? Entre em contato para obter mais créditos.';
+$lang['No matching line found'] = 'Nenhuma linha correspondente encontrada';
+$lang['No worry!'] = 'Não se preocupe!';
+$lang['Not enough credits!'] = 'Créditos insuficientes!';
