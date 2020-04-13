0bc9aa3330aef72fe867d68b530ea3957e0f6080
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-13 21:24:25 +0200

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['no, cancel'] = 'No, cancel·la';
+$lang['open it'] = 'obre-ho';
+$lang['specific fixed price'] = 'preu fix específic';
+$lang['times the base cost per photo'] = 'vegades el cost bàsic per foto';
+$lang['yes, buy'] = 'sí, compro';
+$lang['follow default price (%s credits currently)'] = 'Seguiu el preu per defecte (actualment %s crèdits)';
+$lang['hour(s)'] = 'hore(s)';
+$lang['minute(s)'] = 'minut(s)';
+$lang['Set price'] = 'Establiu el preu';
+$lang['Showing %s to %s of %s lines'] = 'Mostrant de %s a %s de %s línies';
+$lang['Thank you!'] = 'Gràcies!';
+$lang['Users can buy credits'] = 'Els usuaris poden comprar crèdits';
+$lang['You have %d credits left'] = 'Us queden %d crèdits';
+$lang['credit(s) per photo'] = 'credit(s) per foto';
+$lang['credits'] = 'credits';
+$lang['day(s)'] = 'dies';
+$lang['error while buying photo'] = 'Error en comprar la foto';
+$lang['(filtered from %s total lines)'] = '(filtrades d\'un total de %s línies)';
+$lang['Add credits'] = 'Afegiu crèdits';
+$lang['Buy'] = 'Compreu';
+$lang['Buy more credits on your profile page.'] = 'Compreu més crèdits a la vostra pàgina de perfil.';
+$lang['Buy this photo'] = 'Compreu aquesta foto';
+$lang['Credits history'] = 'Historial de crèdits';
+$lang['Credits paid'] = 'Crèdits pagats';
+$lang['Credits spent'] = 'Crèdits gastats';
+$lang['Default cost'] = 'Cost per defecte';
+$lang['Details'] = 'Detalls';
+$lang['Do you want to use %d credits to download this photo?'] = 'Voleu utilitzar %d crèdits per descarregar aquesta foto?';
+$lang['Download available for'] = 'Descàrrega disponible per ';
+$lang['Download now'] = 'Descarregueu ara';
+$lang['Need more? Contact us to get more credits.'] = 'En necessiteu més? Poseu-vos en contacte amb nosaltres per obtenir més crèdits.';
+$lang['No matching line found'] = 'No s\'ha trobat cap línia que coincideixi';
+$lang['No worry!'] = 'No us amoïneu!';
+$lang['Not enough credits!'] = 'No hi ha prou crèdits!';
+$lang['Pay on Paypal.com'] = 'Pagueu en PayPal.com';
+$lang['PayPal account'] = 'Compte de PayPal';
+$lang['Price per credit'] = 'Preu per crèdit';
+$lang['Remove credits'] = 'Elimineu crèdits';
+$lang['Selling credits'] = 'Venent crèdits';
+$lang['Show %s lines'] = 'Mostra %s línies';
+$lang['%u credits spent for %s (size %s)'] = '%u crèdits gastats per %s (mida %s)';
+$lang['%u credits bought on %s (%s %s)'] = '%u crèdits comprats en %s (%s %s)';
+$lang['%d credits taken from your account.'] = 'S\'han agafat %d crèdits del vostre compte.';
