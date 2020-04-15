2f8e033a3da1f15bf03cb67ed2fd50323984b2ab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-15 19:54:03 +0200

    Add es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d credits taken from your account.'] = 'Se han tomado %d créditos de su cuenta.';
+$lang['%u credits bought on %s (%s %s)'] = '%u créditos comprados en %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u créditos gastados por %s (medida %s)';
+$lang['(filtered from %s total lines)'] = '(filtradas de un total de %s líneas)';
+$lang['Add credits'] = 'Añadir créditos';
+$lang['Buy'] = 'Comprar';
+$lang['Buy more credits on your profile page.'] = 'Compre más creditos en su página de perfil.';
+$lang['Buy this photo'] = 'Comprar esta foto';
+$lang['Credits history'] = 'Historial de créditos';
+$lang['Credits paid'] = 'Créditos pagados';
+$lang['Credits spent'] = 'Créditos gastados';
+$lang['Default cost'] = 'Coste por defecto';
+$lang['Details'] = 'Detalles';
+$lang['Do you want to use %d credits to download this photo?'] = '¿Desea utilizar %d créditos para descargar esta foto?';
+$lang['Download available for'] = 'Descarga disponible para';
+$lang['Download now'] = 'Descargar ahora';
+$lang['Need more? Contact us to get more credits.'] = '¿Necesita más? Póngase en contacto con nosotros para obtener más créditos.';
+$lang['No matching line found'] = 'No se ha encontrado ninguna línea que coincida';
+$lang['No worry!'] = '¡No se preocupe!';
+$lang['Not enough credits!'] = '¡No hay bastantes créditos!';
+$lang['Pay on Paypal.com'] = 'Pague en PayPal.com';
+$lang['PayPal account'] = 'Cuenta de PayPal';
+$lang['Price per credit'] = 'Precio por crédito';
+$lang['Remove credits'] = 'Eliminar créditos';
+$lang['Selling credits'] = 'Vendiendo créditos';
+$lang['Set price'] = 'Establezca el precio';
+$lang['Show %s lines'] = 'Mostrar %s líneas';
+$lang['Showing %s to %s of %s lines'] = 'Mostrando de %s a %s de %s líneas';
+$lang['Thank you!'] = '¡Gracias!';
+$lang['Users can buy credits'] = 'Los usuarios pueden comprar créditos';
+$lang['You have %d credits left'] = 'Le quedan %d créditos';
+$lang['credit(s) per photo'] = 'Credito(s) por foto';
+$lang['credits'] = 'créditos';
+$lang['day(s)'] = 'días';
+$lang['error while buying photo'] = 'Error al comprar la foto';
+$lang['follow default price (%s credits currently)'] = 'Siga el precio por defecto (actualmente %s créditos)';
+$lang['hour(s)'] = 'hora(s)';
+$lang['minute(s)'] = 'minuto(s)';
+$lang['no, cancel'] = 'No, cancelar';
+$lang['open it'] = 'Abrirlo';
+$lang['specific fixed price'] = 'Precio fijo específico';
+$lang['times the base cost per photo'] = 'veces el coste básico por foto';
+$lang['yes, buy'] = 'Sí, comprar';
