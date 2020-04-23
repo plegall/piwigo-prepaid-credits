957c6e622f143d2b9c70231672d0e91fc8dba49c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-23 18:31:26 +0200

    Add sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['error while buying photo'] = 'грешка приликом куповине фотографије';
+$lang['follow default price (%s credits currently)'] = 'пратите подразумевану цену (тренутно је %s кредита)';
+$lang['hour(s)'] = 'сат(и)';
+$lang['minute(s)'] = 'минут(а)';
+$lang['no, cancel'] = 'не, откажи';
+$lang['open it'] = 'отвори је';
+$lang['specific fixed price'] = 'одређена, фиксна цена';
+$lang['times the base cost per photo'] = 'пута основна цена по фотографији';
+$lang['yes, buy'] = 'да, купи';
+$lang['Pay on Paypal.com'] = 'Платите на Paypal.com';
+$lang['PayPal account'] = 'PayPal налог';
+$lang['Price per credit'] = 'Цена за кредит';
+$lang['Remove credits'] = 'Уклони кредите';
+$lang['Selling credits'] = 'Кредити за продају';
+$lang['Set price'] = 'Постави цену';
+$lang['Show %s lines'] = 'Прикажи %s редова';
+$lang['Showing %s to %s of %s lines'] = 'Приказујем редове %s до %s од укупно %s';
+$lang['Thank you!'] = 'Хвала вам!';
+$lang['Users can buy credits'] = 'Корисници могу да купе кредите';
+$lang['You have %d credits left'] = 'Имате још %d кредита';
+$lang['credit(s) per photo'] = 'кредит(и) пи слици';
+$lang['credits'] = 'кредити';
+$lang['day(s)'] = 'дан(и)';
+$lang['%d credits taken from your account.'] = '%d кредита је скинуто с вашег налога.';
+$lang['%u credits bought on %s (%s %s)'] = '%u кредита је купљено %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u кредита је потрошено за %s (величина %s)';
+$lang['(filtered from %s total lines)'] = '(филтрирано из укупно %s редова)';
+$lang['Add credits'] = 'Додај кредит';
+$lang['Buy'] = 'Купи';
+$lang['Buy more credits on your profile page.'] = 'Купите још кредита са странице вашег профиле.';
+$lang['Buy this photo'] = 'Купи ову фотографију';
+$lang['Credits history'] = 'Историја кредита';
+$lang['Credits paid'] = 'Плаћени кредит';
+$lang['Credits spent'] = 'Потрошен кредит';
+$lang['Default cost'] = 'Подразумевана цена';
+$lang['Details'] = 'Детаљи';
+$lang['Do you want to use %d credits to download this photo?'] = 'Да ли желите да искористите %d кредита за преузимање ове фотографије?';
+$lang['Download available for'] = 'Преузимање доступно за';
+$lang['Download now'] = 'Преузми сада';
+$lang['Need more? Contact us to get more credits.'] = 'Желите још? Контактирајте нас да добијете додатни кредит.';
+$lang['No matching line found'] = 'Није пронађен овакав ред';
+$lang['No worry!'] = 'Без бриге!';
+$lang['Not enough credits!'] = 'Нема довољно кредита!';
