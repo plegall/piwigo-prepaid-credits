8333c44b183b53ce018238749f1c1fcc8f341b56
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-04-30 18:27:37 +0200

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['no, cancel'] = 'нет, отмена';
+$lang['specific fixed price'] = 'специальная фиксированная цена';
+$lang['yes, buy'] = 'да, покупаю';
+$lang['Thank you!'] = 'Спасибо!';
+$lang['Users can buy credits'] = 'Пользователи могут покупать кредиты';
+$lang['You have %d credits left'] = 'У Вас осталось %d кредита(ов)';
+$lang['credit(s) per photo'] = 'кредит(ов) за изображение';
+$lang['credits'] = 'кредиты';
+$lang['day(s)'] = 'дней ';
+$lang['error while buying photo'] = 'ошибка при покупке изображения';
+$lang['hour(s)'] = 'час(ов)';
+$lang['minute(s)'] = 'минут(а)';
+$lang['No matching line found'] = 'Нет соответствующей найденной линии';
+$lang['Pay on Paypal.com'] = 'Оплата на Paypal.com';
+$lang['PayPal account'] = 'Аккаунт PayPal';
+$lang['Price per credit'] = 'Цена за кредит';
+$lang['Remove credits'] = 'Перемешение кредитов';
+$lang['Selling credits'] = 'Продажа кредитов';
+$lang['Set price'] = 'Установить цену';
+$lang['%d credits taken from your account.'] = '%d кредитов взято с Вашего счета.';
+$lang['(filtered from %s total lines)'] = '(отфильтровано из %s линий)';
+$lang['Add credits'] = 'Добавьте кредиты';
+$lang['Buy'] = 'Купить';
+$lang['Buy more credits on your profile page.'] = 'Купите больше кредитов на странице Вашего профиля.';
+$lang['Buy this photo'] = 'Купить это изображение';
+$lang['Details'] = 'Детали';
+$lang['Download now'] = 'Скачать сейчас';
+$lang['Need more? Contact us to get more credits.'] = 'Нужно больше? Обратитесь к нам, чтобы получить больше кредитов.';
+$lang['No worry!'] = 'Не волнуйтесь!';
+$lang['Not enough credits!'] = 'Кредитов не достаточно!';
