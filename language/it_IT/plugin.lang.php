398ac84654484a7b50332e63b94801bb9aadf5ae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-08 17:26:45 +0200

    Add it_IT, thanks to virgigiole

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['hour(s)'] = 'ora/e';
+$lang['minute(s)'] = 'minuto/i';
+$lang['times the base cost per photo'] = 'per il costo base per foto';
+$lang['You have %d credits left'] = 'Ti restano %d crediti';
+$lang['Price per credit'] = 'Prezzo per credito';
+$lang['Download available for'] = 'Download disponibile per';
+$lang['Not enough credits!'] = 'Crediti insufficienti';
+$lang['Credits history'] = 'Storico crediti';
+$lang['Credits paid'] = 'Crediti pagati';
+$lang['Credits spent'] = 'Crediti spesi';
+$lang['Thank you!'] = 'Grazie!';
+$lang['no, cancel'] = 'no, cancella';
+$lang['yes, buy'] = 'si, acquista';
+$lang['Set price'] = 'Imposta prezzo';
+$lang['Pay on Paypal.com'] = 'Paga su Paypal.com';
+$lang['PayPal account'] = 'Acount PayPal';
+$lang['Buy'] = 'Acquista';
+$lang['Buy this photo'] = 'Acquista questa foto';
+$lang['Details'] = 'Dettagli';
+$lang['Download now'] = 'Scarica adesso';
