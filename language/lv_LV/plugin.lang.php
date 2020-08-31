6d604d212e3b51085f1d718712225918d392c018
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-08-31 14:30:29 +0200

    Add lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Do you want to use %d credits to download this photo?'] = 'Vai gribat izmantot %d kredītu, lai lejupielādētu šo foto? ';
+$lang['You have %d credits left'] = 'Jums vēl atlikuši %d kredīti';
+$lang['Price per credit'] = 'kredīta cena';
+$lang['specific fixed price'] = 'noteikt fiksētu cenu';
+$lang['Need more? Contact us to get more credits.'] = 'Vajag vēl? Sazinieties ar mums, lai iegādātos vairāk kredītu.';
+$lang['%d credits taken from your account.'] = '%d kredītu noņemti no jūsu konta.';
+$lang['Download available for'] = 'Lejupielāde pieejama priekš';
+$lang['Buy more credits on your profile page.'] = 'Pirkt vairāk kredītu jūsu profila lapā';
+$lang['error while buying photo'] = 'kļuda foto pirkšanas laikā';
+$lang['minute(s)'] = 'minūte(s)';
+$lang['hour(s)'] = 'stunda(s)';
+$lang['Users can buy credits'] = 'Lietotāji var pirkt kredītus';
+$lang['Selling credits'] = 'kredītu pārdošana';
+$lang['yes, buy'] = 'jā, pirkt';
+$lang['open it'] = 'atvērt to';
+$lang['no, cancel'] = 'nē, atcelt';
+$lang['day(s)'] = 'diena(s)';
+$lang['credits'] = 'kredīti';
+$lang['Pay on Paypal.com'] = 'Maksāt ar Paypal.com';
+$lang['Set price'] = 'Noteikt cenu';
+$lang['Thank you!'] = 'Paldies';
+$lang['Remove credits'] = 'Dzēst kredītu';
+$lang['PayPal account'] = 'Paypal konts';
+$lang['Download now'] = 'Lejupielādēt tagad';
+$lang['Not enough credits!'] = 'Nav pietiekoši kredītu';
+$lang['No worry!'] = 'Bez uztraukumiem!';
+$lang['Details'] = 'Detaļas';
+$lang['Default cost'] = 'Cena pēc noklusējuma';
+$lang['Buy'] = 'Pirkt';
+$lang['Buy this photo'] = 'Nopirkt šo foto';
+$lang['Credits paid'] = 'Samaksātie kredīti';
+$lang['Credits spent'] = 'Iztērētie kredīti';
+$lang['Credits history'] = 'Kredītvēsture';
+$lang['Add credits'] = 'Pievienot kredītu';
