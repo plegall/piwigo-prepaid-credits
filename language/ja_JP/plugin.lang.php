af0dea1897f22d3b9b6b2e21004012bd0575e111
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-09-04 18:34:29 +0200

    Add ja_JP, thanks to ryoon

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = '特定の固定価格';
+$lang['times the base cost per photo'] = '写真ごとのベースコストの倍数';
+$lang['yes, buy'] = '購入します';
+$lang['%d credits taken from your account.'] = 'あなたのアカウントから%dクレジットを取得します。';
+$lang['%u credits bought on %s (%s %s)'] = '%u クレジットを%s (%s %s)で購入します。';
+$lang['%u credits spent for %s (size %s)'] = '%u クレジットを%s (size %s)に利用します。';
+$lang['(filtered from %s total lines)'] = '(全%s行から絞り込んでいます)';
+$lang['Add credits'] = 'クレジットを追加する';
+$lang['Credits history'] = 'クレジットの履歴';
+$lang['Credits paid'] = '支払ったクレジット';
+$lang['Credits spent'] = '利用したクレジット';
+$lang['Default cost'] = 'デフォルトのコスト';
+$lang['Details'] = '詳細';
+$lang['Do you want to use %d credits to download this photo?'] = 'この写真をダウンロードするのに%dクレジットを利用しますか?';
+$lang['Download available for'] = '以下がダウンロード可能です:';
+$lang['Download now'] = '今すぐダウンロードする';
+$lang['Need more? Contact us to get more credits.'] = 'もっと必要ですか? よりクレジットを入手するには私たちに連絡してください。';
+$lang['No matching line found'] = '適合する行はありません';
+$lang['Pay on Paypal.com'] = 'PayPal.comえ支払う';
+$lang['PayPal account'] = 'PayPalアカウント';
+$lang['Price per credit'] = 'クレジットごとの価格';
+$lang['Remove credits'] = 'クレジットを削除する';
+$lang['Selling credits'] = '販売するクレジット';
+$lang['Show %s lines'] = '%s 行表示する';
+$lang['Showing %s to %s of %s lines'] = '%s から %s 行目を表示する(全 %s 行)';
+$lang['credit(s) per photo'] = '写真ごとのクレジット';
+$lang['error while buying photo'] = '写真を購入する際のエラー';
+$lang['follow default price (%s credits currently)'] = 'デフォルトの価格に従う(現在は %s クレジットです)';
+$lang['open it'] = '開く';
+$lang['Set price'] = '価格を設定する';
+$lang['hour(s)'] = '時間';
+$lang['minute(s)'] = '分数';
+$lang['no, cancel'] = 'キャンセルする';
+$lang['Users can buy credits'] = 'ユーザーはクレジットを購入できます。';
+$lang['Thank you!'] = 'ありがとうございます!';
+$lang['day(s)'] = '日間';
+$lang['credits'] = 'を購入する';
+$lang['Buy'] = '以下の';
+$lang['You have %d credits left'] = 'あなたは %d 分クレジットを持っています。';
+$lang['Buy more credits on your profile page.'] = 'あなたのプロフィールページでクレジットを購入できます。';
+$lang['No worry!'] = '心配いりません!';
+$lang['Not enough credits!'] = 'クレジットが十分にありません!';
+$lang['Buy this photo'] = 'この写真を購入する';
