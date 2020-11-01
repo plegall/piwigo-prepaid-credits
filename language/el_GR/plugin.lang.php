7425adf9f070d38b4d1aa5ac7ddbdc46a659773d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-11-01 12:42:30 +0100

    Add el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Default cost'] = 'Προεπιλεγμένο κόστος';
+$lang['Credits spent'] = 'Πιστώσεις που δαπανήθηκαν';
+$lang['Credits paid'] = 'Καταβληθείσες πιστώσεις';
+$lang['Credits history'] = 'Ιστορικό πιστώσεων';
+$lang['Buy this photo'] = 'Αγοράστε αυτήν τη φωτογραφία';
+$lang['Buy more credits on your profile page.'] = 'Αγοράστε περισσότερες πιστώσεις στη σελίδα του προφίλ σας.';
+$lang['Buy'] = 'Αγορά';
+$lang['Add credits'] = 'Προσθέστε πιστώσεις';
+$lang['%u credits spent for %s (size %s)'] = '%u πιστώσεις που δαπανήθηκαν για %s (Μέγεθος %s)';
+$lang['(filtered from %s total lines)'] = '(φιλτράρεται από %s συνολικές γραμμές)';
+$lang['%u credits bought on %s (%s %s)'] = '%u πιστώσεις που αγοράστηκαν στο %s (%s %s)';
+$lang['%d credits taken from your account.'] = '%d πιστώσεις που αφαιρέθηκαν από τον λογαριασμό σας.';
