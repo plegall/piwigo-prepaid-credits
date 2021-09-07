bc1c5c33c72f88551e46899e7459f5f65c71fb87
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-09-07 12:24:48 +0200

    Update hu_HU, thanks to mb_wG2ofaWVb9W4wCu

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = 'vonatkozó fix ár';
+$lang['times the base cost per photo'] = 'szorozva az alapárral';
+$lang['yes, buy'] = 'igen, megveszem';
+$lang['open it'] = 'megnyitás';
+$lang['Pay on Paypal.com'] = 'Fizetés PayPallal';
+$lang['PayPal account'] = 'PayPal fiók';
+$lang['Price per credit'] = 'Ár kreditenként';
+$lang['Remove credits'] = 'Kreditek eltávolítása';
+$lang['Selling credits'] = 'Kreditek értékesítése';
+$lang['Set price'] = 'Beállított ár';
+$lang['Show %s lines'] = '%s sor mutatása';
+$lang['Showing %s to %s of %s lines'] = 'Sorok: %s - %s (összes: %s)';
+$lang['Thank you!'] = 'Köszönjük!';
+$lang['Users can buy credits'] = 'A felhasználók vásárolhatnak kreditet';
+$lang['You have %d credits left'] = '%d kredited maradt';
+$lang['credit(s) per photo'] = 'Kredit fontónként';
+$lang['credits'] = 'kreditek';
+$lang['day(s)'] = 'nap';
+$lang['error while buying photo'] = 'hiba a fotó megvásárlásakor';
+$lang['follow default price (%s credits currently)'] = 'alapértelmezett ár követése (%s kredit jelenleg)';
+$lang['hour(s)'] = 'óra';
+$lang['minute(s)'] = 'perc';
+$lang['no, cancel'] = 'nem, mégsem';
+$lang['%d credits taken from your account.'] = '%d kreditet vontunk le a számládról';
+$lang['%u credits bought on %s (%s %s)'] = '%u kreditet vásároltál ekkor: %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u kreditet költöttél erre: %s (méret %s)';
+$lang['(filtered from %s total lines)'] = '(szűrt lista, %s összesen)';
+$lang['Add credits'] = 'Kreditek hozzáadása';
+$lang['Buy'] = 'Vásárlás';
+$lang['Buy more credits on your profile page.'] = 'A profiloldalon vásárolhatsz kreditet';
+$lang['Buy this photo'] = 'Kép megvásárlása';
+$lang['Credits history'] = 'Kredittörténet';
+$lang['Credits paid'] = 'Kifizetett kredit';
+$lang['Credits spent'] = 'Elköltött kredit';
+$lang['Default cost'] = 'Alapértelmezett ár';
+$lang['Details'] = 'Részletek';
+$lang['Do you want to use %d credits to download this photo?'] = 'A fotó letöltése %d kreditbe kerül. Mehet?';
+$lang['Download available for'] = 'Letöltés elérhető:';
+$lang['Download now'] = 'Letöltés';
+$lang['Need more? Contact us to get more credits.'] = 'Több kell? Írj nekünk további kreditekért';
+$lang['No matching line found'] = 'Nincs egyezés';
+$lang['No worry!'] = 'Ne aggódj!';
+$lang['Not enough credits!'] = 'Nincs elég kredit!';
