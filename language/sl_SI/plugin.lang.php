129e56f1028571eee484219cb4577f25aed795d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-15 20:46:36 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%u credits spent for %s (size %s)'] = '%u kreditov, porabljenih za %s (velikost %s)';
+$lang['open it'] = 'odprite';
+$lang['%u credits bought on %s (%s %s)'] = '%u kreditov kupljenih na %s (%s %s)';
+$lang['Credits history'] = 'Kreditna zgodovina';
+$lang['Pay on Paypal.com'] = 'Plačajte na Paypal.com';
+$lang['Buy'] = 'Kupite';
+$lang['You have %d credits left'] = 'Imate še %d kreditov';
+$lang['Not enough credits!'] = 'Premalo kreditov!';
+$lang['Buy more credits on your profile page.'] = 'Kupite več kreditov na strani profila.';
+$lang['No worry!'] = 'Brez skrbi!';
+$lang['Need more? Contact us to get more credits.'] = 'Potrebujemo več? Kontaktirajte nas, če želite pridobiti več kreditov.';
+$lang['error while buying photo'] = 'napaka pri nakupu fotografije';
+$lang['Download now'] = 'Prenesi zdaj';
+$lang['%d credits taken from your account.'] = '%d kreditov, vzetih z vašega računa.';
+$lang['Thank you!'] = 'Hvala vam!';
+$lang['no, cancel'] = 'ne, prekliči';
+$lang['yes, buy'] = 'da, kupi';
+$lang['Do you want to use %d credits to download this photo?'] = 'Ali želite uporabiti %d kreditov za prenos te fotografije?';
+$lang['Buy this photo'] = 'Kupi to fotografijo';
+$lang['Remove credits'] = 'Odstranite kredite';
+$lang['Add credits'] = 'Dodajte kredite';
+$lang['credits'] = 'krediti';
+$lang['specific fixed price'] = 'določena fiksna cena';
+$lang['follow default price (%s credits currently)'] = 'sledi privzeti ceni (trenutno %s kreditov)';
+$lang['Set price'] = 'Določite ceno';
+$lang['(filtered from %s total lines)'] = '(filtrirano iz %s skupnih vrstic)';
+$lang['Showing %s to %s of %s lines'] = 'Prikaz %s do %s od %s vrstic';
+$lang['No matching line found'] = 'Ustrezna vrstica ni bila najdena';
+$lang['Show %s lines'] = 'Prikaži %s vrstic';
+$lang['Details'] = 'Podrobnosti';
+$lang['Credits paid'] = 'Plačani krediti';
+$lang['Credits spent'] = 'Porabljeni krediti';
+$lang['PayPal account'] = 'PayPal račun';
+$lang['Price per credit'] = 'Cena na kredit';
+$lang['Users can buy credits'] = 'Uporabniki lahko kupijo kredite';
+$lang['Selling credits'] = 'Prodaja kreditov';
+$lang['times the base cost per photo'] = 'krat osnovni strošek na sliko';
+$lang['day(s)'] = 'dan(i)';
+$lang['hour(s)'] = 'ura(e)';
+$lang['minute(s)'] = 'minuta(e';
+$lang['Download available for'] = 'Prenos je na voljo za';
+$lang['credit(s) per photo'] = 'kredit(a) na sliko';
+$lang['Default cost'] = 'Privzeta cena';
