742e7fffdade2a478815e12a4de6472f8693947f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-26 05:49:57 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = 'tiltekið fast verð';
+$lang['times the base cost per photo'] = 'sinnum grunnkostnaður á hverja mynd';
+$lang['yes, buy'] = 'já, kaupa';
+$lang['Pay on Paypal.com'] = 'Borgaðu á Paypal.com';
+$lang['PayPal account'] = 'PayPal reikningur';
+$lang['Price per credit'] = 'Verð á inneign';
+$lang['Remove credits'] = 'Fjarlægja inneign';
+$lang['Selling credits'] = 'Selja inneign';
+$lang['Set price'] = 'Verðsetja';
+$lang['Show %s lines'] = 'Sýna %s línur';
+$lang['Showing %s to %s of %s lines'] = 'Sýni %s til %s af %s línum';
+$lang['Thank you!'] = 'Takk fyrir!';
+$lang['Users can buy credits'] = 'Notendur geta keypt inneign';
+$lang['You have %d credits left'] = 'Þú átt %d inneignir eftir';
+$lang['credit(s) per photo'] = 'inneign(ir) á mynd';
+$lang['credits'] = 'inneignir';
+$lang['day(s)'] = 'dag(a)';
+$lang['error while buying photo'] = 'villa við innkaup á mynd';
+$lang['follow default price (%s credits currently)'] = 'fylgja sjálfgefnu verði (%s núverandi inneignir)';
+$lang['hour(s)'] = 'klukkustund(ir)';
+$lang['minute(s)'] = 'mínúta(ur)';
+$lang['no, cancel'] = 'nei, hætta við';
+$lang['open it'] = 'opna það';
+$lang['%d credits taken from your account.'] = '%d inneign tekin af reikningnum þínum.';
+$lang['%u credits bought on %s (%s %s)'] = '%u inneign keypt þann %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u inneignum varið fyrir %s (stærð %s)';
+$lang['(filtered from %s total lines)'] = '(síað frá %s heildarlínum)';
+$lang['Add credits'] = 'Bæta við inneign';
+$lang['Buy'] = 'Kaupa';
+$lang['Buy more credits on your profile page.'] = 'Kauptu meiri inneign á prófílsíðunni þinni.';
+$lang['Buy this photo'] = 'Kaupa þessa mynd';
+$lang['Credits history'] = 'Inneignarsaga';
+$lang['Credits paid'] = 'Inneign greidd';
+$lang['Credits spent'] = 'Inneign eytt';
+$lang['Default cost'] = 'Sjálfgefið verð';
+$lang['Details'] = 'Upplýsingar';
+$lang['Do you want to use %d credits to download this photo?'] = 'Viltu nota %d inneign til að hlaða niður þessari mynd?';
+$lang['Download available for'] = 'Niðurhal í boði fyrir';
+$lang['Download now'] = 'Hlaða niður núna';
+$lang['Need more? Contact us to get more credits.'] = 'Þarftu meira? Hafðu samband við okkur til að fá meiri inneign.';
+$lang['No matching line found'] = 'Engin samsvarandi lína fannst';
+$lang['No worry!'] = 'Engar áhyggjur!';
+$lang['Not enough credits!'] = 'Ekki næg inneign!';
