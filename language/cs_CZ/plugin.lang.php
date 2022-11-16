dbec013187930c42b4ee02c521dd2c718df8564c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-16 06:22:28 +0100

    Update cs_CZ, thanks to gandycz

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = 'konkrétní pevná cena';
+$lang['times the base cost per photo'] = 'násobek základní ceny za fotografii';
+$lang['yes, buy'] = 'ano, koupit';
+$lang['Pay on Paypal.com'] = 'Plaťte na Paypal.com';
+$lang['PayPal account'] = 'Paypal účet';
+$lang['Price per credit'] = 'Cena za kredit';
+$lang['Remove credits'] = 'Odebrat kredity';
+$lang['Selling credits'] = 'Prodej kreditů';
+$lang['Set price'] = 'Nastavit cenu';
+$lang['Show %s lines'] = 'Zobrazit %s řádků';
+$lang['Showing %s to %s of %s lines'] = 'Zobrazuje se %s až %s z %s řádků';
+$lang['Thank you!'] = 'Děkujeme vám!';
+$lang['Users can buy credits'] = 'Uživatelé mohou nakupovat kredity';
+$lang['You have %d credits left'] = 'Zbývají vám %d kredity';
+$lang['credit(s) per photo'] = 'kredit(y) za fotografii';
+$lang['credits'] = 'kredity';
+$lang['day(s)'] = 'den(dny)';
+$lang['error while buying photo'] = 'chyba při nákupu fotografie';
+$lang['follow default price (%s credits currently)'] = 'následovat výchozí cenu (aktuálně %s kreditů)';
+$lang['hour(s)'] = 'hodina(y)';
+$lang['minute(s)'] = 'minuta(y)';
+$lang['no, cancel'] = 'ne, zrušit';
+$lang['open it'] = 'otevři to';
+$lang['%d credits taken from your account.'] = '%d kreditů odebráno z vašeho účtu.';
+$lang['%u credits bought on %s (%s %s)'] = '%u kreditů zakoupených na %s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u kreditů vynaložených za %s (velikost %s)';
+$lang['(filtered from %s total lines)'] = '(filtrováno z %s celkových řádků)';
+$lang['Add credits'] = 'Přidat kredity';
+$lang['Buy'] = 'Koupit';
+$lang['Buy more credits on your profile page.'] = 'Koupit více kreditů na stránce profilu.';
+$lang['Buy this photo'] = 'Koupit tuto fotografii';
+$lang['Credits history'] = 'Historie kreditů';
+$lang['Credits paid'] = 'Kredity zaplaceny';
+$lang['Credits spent'] = 'Utracené kredity';
+$lang['Default cost'] = 'Výchozí cena';
+$lang['Details'] = 'Podrobnosti';
+$lang['Do you want to use %d credits to download this photo?'] = 'Chcete použít %d kreditů ke stažení této fotografie?';
+$lang['Download available for'] = 'K dispozici ke stažení pro';
+$lang['Download now'] = 'Stáhnout nyní';
+$lang['Need more? Contact us to get more credits.'] = 'Potřebovat více? Kontaktujte nás a získejte více kreditů.';
+$lang['No matching line found'] = 'Nebyl nalezen žádný odpovídající řádek';
+$lang['No worry!'] = 'Žádný strach!';
+$lang['Not enough credits!'] = 'Nedostatek kreditů!';
