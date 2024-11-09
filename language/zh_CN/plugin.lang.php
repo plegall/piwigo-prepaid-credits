3aacbd551bcafa39918ac5463cc1591a3945fec7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-09 05:11:15 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = '特定固定价格';
+$lang['times the base cost per photo'] = '每张照片的基础成本倍数';
+$lang['yes, buy'] = '是的，购买';
+$lang['Pay on Paypal.com'] = '在 Paypal.com 上支付';
+$lang['PayPal account'] = 'PayPal 账户';
+$lang['Price per credit'] = '每个积分的价格';
+$lang['Remove credits'] = '删除积分';
+$lang['Selling credits'] = '出售积分';
+$lang['Set price'] = '设置价格';
+$lang['Show %s lines'] = '显示 %s 行';
+$lang['Showing %s to %s of %s lines'] = '显示第 %s 到第 %s 行，共 %s 行';
+$lang['Thank you!'] = '谢谢！';
+$lang['Users can buy credits'] = '用户可以购买积分';
+$lang['You have %d credits left'] = '您还有 %d 积分';
+$lang['credit(s) per photo'] = '每张照片的积分';
+$lang['credits'] = '积分';
+$lang['day(s)'] = '天';
+$lang['error while buying photo'] = '购买照片时出错';
+$lang['follow default price (%s credits currently)'] = '遵循默认价格（当前为 %s 积分）';
+$lang['hour(s)'] = '小时';
+$lang['minute(s)'] = '分钟';
+$lang['no, cancel'] = '不，取消';
+$lang['open it'] = '打开';
+$lang['%d credits taken from your account.'] = '%d 积分已从您的帐户中扣除。';
+$lang['%u credits bought on %s (%s %s)'] = '%u 积分已购买于 %s（%s %s）';
+$lang['%u credits spent for %s (size %s)'] = '您花费了 %u 积分用于 %s (大小 %s)';
+$lang['(filtered from %s total lines)'] = '（从 %s 总行数中筛选）';
+$lang['Add credits'] = '添加积分';
+$lang['Buy'] = '购买';
+$lang['Buy more credits on your profile page.'] = '在您的个人资料页面上购买更多积分';
+$lang['Buy this photo'] = '购买这张照片';
+$lang['Credits history'] = '积分历史';
+$lang['Credits paid'] = '已支付的积分';
+$lang['Credits spent'] = '已花费的积分';
+$lang['Default cost'] = '默认费用';
+$lang['Details'] = '详情';
+$lang['Do you want to use %d credits to download this photo?'] = '您想使用 %d 积分下载这张照片吗？';
+$lang['Download available for'] = '可下载';
+$lang['Download now'] = '立即下载';
+$lang['Need more? Contact us to get more credits.'] = '需要更多？联系我们获取更多积分';
+$lang['No matching line found'] = '未找到匹配的行';
+$lang['No worry!'] = '不用担心！';
+$lang['Not enough credits!'] = '积分不足！';
