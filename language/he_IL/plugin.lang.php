19cc3b744376c84026d38487cead51d0175bbb17
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:16:24 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d credits taken from your account.'] = '%d נקודות זכות נלקחו מהחשבון שלך.';
+$lang['%u credits bought on %s (%s %s)'] = '%u נקודות זכות נרכשו ב-%s (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u נקודות זכות נוצלו עבור %s (גודל %s)';
+$lang['(filtered from %s total lines)'] = '(סונן מתוך %s קווים כוללים)';
+$lang['Add credits'] = 'הוסף נקודות זכות';
+$lang['Buy'] = 'קנה';
+$lang['Buy more credits on your profile page.'] = 'קנה עוד נקודות זכות בדף הפרופיל שלך.';
+$lang['Buy this photo'] = 'קנה את התמונה הזו';
+$lang['Credits history'] = 'היסטוריית נקודות זכות';
+$lang['Credits paid'] = 'נקודות זכות ששולמו';
+$lang['Credits spent'] = 'נקודות זכות שנוצלו';
+$lang['Default cost'] = 'עלות ברירת מחדל';
+$lang['Details'] = 'פרטים';
+$lang['Do you want to use %d credits to download this photo?'] = 'האם ברצונך להשתמש ב-%d נקודות זכות להורדת התמונה הזו?';
+$lang['Download available for'] = 'הורדה זמינה עבור';
+$lang['Download now'] = 'הורד עכשיו';
+$lang['Need more? Contact us to get more credits.'] = 'צריך עוד? צור קשר כדי לקבל עוד נקודות זכות.';
+$lang['No matching line found'] = 'לא נמצאה שורה תואמת';
+$lang['No worry!'] = 'אין צורך לדאוג!';
+$lang['Not enough credits!'] = 'לא מספיק נקודות זכות!';
+$lang['Pay on Paypal.com'] = 'שלם ב-Paypal.com';
+$lang['PayPal account'] = 'חשבון PayPal';
+$lang['Price per credit'] = 'מחיר לנקודת זכות';
+$lang['Remove credits'] = 'הסר נקודות זכות';
+$lang['Selling credits'] = 'מכירת נקודות זכות';
+$lang['Set price'] = 'הגדר מחיר';
+$lang['Show %s lines'] = 'הצג %s שורות';
+$lang['Showing %s to %s of %s lines'] = 'מציג %s עד %s מתוך %s שורות';
+$lang['Thank you!'] = 'תודה!';
+$lang['Users can buy credits'] = 'המשתמשים יכולים לקנות נקודות זכות';
+$lang['You have %d credits left'] = 'יש לך עוד %d נקודות זכות';
+$lang['credit(s) per photo'] = 'נקודה(ות) לכל תמונה';
+$lang['credits'] = 'נקודות זכות';
+$lang['day(s)'] = 'יום(ים)';
+$lang['error while buying photo'] = 'שגיאה בעת רכישת התמונה';
+$lang['follow default price (%s credits currently)'] = 'עקוב אחרי המחיר המוגדר כברירת מחדל (%s נקודות זכות כרגע)';
+$lang['hour(s)'] = 'שעה(ות)';
+$lang['minute(s)'] = 'דקה(ות)';
+$lang['no, cancel'] = 'לא, בטל';
+$lang['open it'] = 'פתח את זה';
+$lang['specific fixed price'] = 'מחיר קבוע ספציפי';
+$lang['times the base cost per photo'] = 'פעמים מעלות העלות הבסיסית לתמונה';
+$lang['yes, buy'] = 'כן, קנה';
