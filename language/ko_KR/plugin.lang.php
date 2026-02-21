06e56a466e606c29d778453218bd8793fdd84f8f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-21 06:21:36 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['specific fixed price'] = '특정 고정 가격';
+$lang['times the base cost per photo'] = '사진당 기본 비용에 곱하기';
+$lang['yes, buy'] = '예, 구매';
+$lang['Pay on Paypal.com'] = 'PayPal.com에서 결제하기';
+$lang['PayPal account'] = 'PayPal 계정';
+$lang['Price per credit'] = '크레딧당 가격';
+$lang['Remove credits'] = '크레딧 제거';
+$lang['Selling credits'] = '크레딧 판매';
+$lang['Set price'] = '가격 설정';
+$lang['Show %s lines'] = '%s 줄 표시';
+$lang['Showing %s to %s of %s lines'] = '%s부터 %s까지 (전체 %s 줄)표시 중';
+$lang['Thank you!'] = '감사합니다!';
+$lang['Users can buy credits'] = '사용자는 크레딧을 구매할 수 있습니다';
+$lang['You have %d credits left'] = '현재 %d 크레딧이 남아 있습니다';
+$lang['credit(s) per photo'] = '사진당 크레딧';
+$lang['credits'] = '크레딧';
+$lang['day(s)'] = '일';
+$lang['error while buying photo'] = '사진 구매 중 오류 발생';
+$lang['follow default price (%s credits currently)'] = '기본 가격 적용 (%s 크레딧 현재)';
+$lang['hour(s)'] = '
+시간';
+$lang['minute(s)'] = '분';
+$lang['no, cancel'] = '아니요, 취소';
+$lang['open it'] = '열기';
+$lang['%d credits taken from your account.'] = '%d 크레딧이 계정에서 차감되었습니다.';
+$lang['%u credits bought on %s (%s %s)'] = '%u 크레딧을 %s에서 구매했습니다 (%s %s)';
+$lang['%u credits spent for %s (size %s)'] = '%u 크레딧을 %s (크기 %s)에 사용했습니다';
+$lang['(filtered from %s total lines)'] = '(총 %s 줄 중 필터링됨)';
+$lang['Add credits'] = '크레딧 추가';
+$lang['Buy'] = '구매';
+$lang['Buy more credits on your profile page.'] = '프로필 페이지에서 크레딧을 더 구매하세요.';
+$lang['Buy this photo'] = '이 사진 구매하기';
+$lang['Credits history'] = '크레딧 내역';
+$lang['Credits paid'] = '결제된 크레딧';
+$lang['Credits spent'] = '사용된 크레딧';
+$lang['Default cost'] = '기본 비용';
+$lang['Details'] = '상세 정보';
+$lang['Do you want to use %d credits to download this photo?'] = '이 사진의 다운로드에 %d 크레딧을 사용하시겠습니까?';
+$lang['Download available for'] = '다운로드 가능 기간';
+$lang['Download now'] = '지금 다운로드';
+$lang['Need more? Contact us to get more credits.'] = '추가 크레딧이 필요하신가요? 문의하여 더 많은 크레딧을 받으세요.';
+$lang['No matching line found'] = '일치하는 항목이 없습니다';
+$lang['No worry!'] = '걱정 마세요!';
+$lang['Not enough credits!'] = '크레딧이 부족합니다!';
